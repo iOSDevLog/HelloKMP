6c9fe619fa97274b70c57285c6f359ae4c1a4974
iosdevlog <iosdevlog@iosdevlog.com>
2020-12-11 13:55:37 +0800

    androidApp

--- androidApp/src/main/java/com/iosdevlog/hellokmp/MainActivity.kt
+++ androidApp/src/main/java/com/iosdevlog/hellokmp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.iosdevlog.hellokmp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iosdevlog.Greeting
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greeting.text = Greeting().greeting()
     }
 }
